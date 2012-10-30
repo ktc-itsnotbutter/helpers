34783cb1938d90121840ba62bcb41501cf5a99ea
Jesse Nelson <spheromak@gmail.com>
2012-10-29 18:16:52 -0700

    release 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email  "jasonjackson@gmail.com"
 license           "All rights reserved"
 description       "Installs/Configures helper methods / libs for recipes"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.2.3"
+version           "1.0.1"
 
 depends "chef_handler"
